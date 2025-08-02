d97b63fc00941357e6db960c3316a77797bdbd89
Adan Zweig <153219904+adan-volt@users.noreply.github.com>
2025-08-02 00:16:37 -0300

    products

--- src/controllers/ProductController.ts
+++ src/controllers/ProductController.ts
@@ -42,6 +42,7 @@ export const store = async (req: Request, res: Response): Promise<Response> => {
       details: req.body.details,
       price: req.body.price,
       stock: req.body.stock,
+      companyId: req.body.companyId,
       image: req.file ? req.file.filename : null, // store filename or URL
     });
     return res.status(201).json(product);
